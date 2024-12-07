b552ac97927288387d9866a4d20e97a546b58b83
Kamal Pro <mohamadkamalzakaria@gmail.com>
2024-12-07 10:12:01 +0800

    remove .next folder add dockerfile add github actions yml disable husky

--- .next/server/app/_not-found/page.js
+++
@@ -1,9 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/_not-found/page.js";
-const runtime = require("../../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_next_8bf18a._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_@auth_core_3d8c01._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_jose_dist_node_esm_e43cb3._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_4c7b6e._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__d589dd._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/_not-found/page/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-page.js?page=/_not-found/page { COMPONENT_0 => \"[project]/app/layout.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_1 => \"[project]/app/not-found.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_2 => \"[project]/app/not-found.tsx [app-rsc] (ecmascript, Next.js server component)\" } [app-rsc] (ecmascript) <facade>", CHUNK_PUBLIC_PATH).exports;
--- app/(auth)/_components/user-auth-form.tsx
+++ app/(auth)/_components/user-auth-form.tsx
@@ -81,13 +81,13 @@ export default function UserAuthForm() {
         <div className="absolute inset-0 flex items-center">
           <span className="w-full border-t" />
         </div>
-        {/* <div className="relative flex justify-center text-xs uppercase">
+        <div className="relative flex justify-center text-xs uppercase">
           <span className="bg-background px-2 text-muted-foreground">
             Or continue with
           </span>
-        </div> */}
+        </div>
       </div>
-      {/* <GithubSignInButton /> */}
+      <GithubSignInButton />
     </>
   );
 }
